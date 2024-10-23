251b07c937d0caab2662e9b8769e1117d0d99a03
Romain Dieu <romain.dieu@peoplbrain.com>
2024-10-23 10:10:30 +0200

    Update Items.py

--- classes/Items.py
+++ classes/Items.py
@@ -18,3 +18,14 @@ class Item:
                  gear_score_min, gear_score_max, has_random_perks, perks_bucket, name, description):
         self.__id = id
         self.__item_type = item_type
+        self._perks = perks
+        self.__icon = icon
+        self.__tier = tier
+        self.__rarity = rarity
+        self.__gear_score = gear_score
+        self.__gear_score_min = gear_score_min
+        self.__gear_score_max = gear_score_max
+        self.__has_random_perks = has_random_perks
+        self.__perks_bucket = perks_bucket
+        self.name = name
+        self.description = description
