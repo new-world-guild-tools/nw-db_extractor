0fff7245834e0ad1e34fcfc16efa592d415892cc
Bambijow <romain.dieu258@gmail.com>
2024-10-25 11:54:24 +0200

    :recycle: feat(Data to api): Refacto Loader class :recycle:
    - Refacto Loader class

--- utils/Loader.py
+++ utils/Loader.py
@@ -16,18 +16,24 @@ class Loader:
         self.__connection = http.client.HTTPSConnection(os.getenv("API_URL"))
         self.__username = os.getenv("API_USERNAME")
         self.__password = os.getenv("API_PASSWORD")
+        self.__connection_token = None
+
+    def __send_request(self, request_type, endpoint, headers, data: None):
+        self.__connection.request(request_type, endpoint, data, headers)
+        return self.__connection.getresponse()
 
     def __stop_connection(self):
         self.__connection.close()
 
-    def __send_post_request(self, endpoint, post_data, headers):
-        self.__connection.request("POST", endpoint, post_data, headers)
-        return self.__connection.getresponse()
+    def send_post_request(self, endpoint, post_data, headers):
+        return self.__send_request("POST", endpoint, headers, post_data)
 
-    def __send_get_request(self, endpoint, headers):
-        self.__connection.request("GET", endpoint, None, headers)
-        return self.__connection.getresponse()
+    def send_get_request(self, endpoint, headers):
+        return self.__send_request("POST", endpoint, headers, None)
 
-    def connect_to_api(self):
+    def send_patch_request(self, endpoint, patch_data, headers):
+        return self.__send_request("POST", endpoint, headers, patch_data)
+
+    def __connect_to_api(self):
         print("Username: " + str(self.__username))
         print("Password: " + str(self.__password))
